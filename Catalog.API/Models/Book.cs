02533a698bee084e1134be334fce2cd75710491e
eddygarros <eddygarros@hotmail.com>
2022-01-31 09:41:43 -0600

    fix: fix

--- Catalog.API/Models/Book.cs
+++ Catalog.API/Models/Book.cs
@@ -1,20 +1,21 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace Catalog.API.Models;
-
-public class Book
+namespace Catalog.API.Models
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
 
-    [BsonElement("Name")]
-    public string BookName { get; set; } = null!;
+        [BsonElement("Name")]
+        public string BookName { get; set; }
 
-    public decimal Price { get; set; }
+        public decimal Price { get; set; }
 
-    public string Category { get; set; } = null!;
+        public string Category { get; set; }
 
-    public string Author { get; set; } = null!;
+        public string Author { get; set; }
+    }
 }
